bcda08f27de8b4835071786436cef1b7c0ba7623
shashank <268678722+shashank601@users.noreply.github.com>
2026-03-19 14:28:37 +0530

    Create 268. Missing Number.cpp

---
+++ Bit Manipulation/268. Missing Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
+
+
+1^2^3^4^...^n^nums[0]^nums[1]^nums[2]..
