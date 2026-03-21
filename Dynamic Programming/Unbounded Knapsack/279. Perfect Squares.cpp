3f6bba7f4225b491b113603bb27dbc5255c1792d
shashank <268678722+shashank601@users.noreply.github.com>
2026-03-21 19:49:16 +0530

    Create 279. Perfect Squares.cpp

---
+++ Dynamic Programming/Unbounded Knapsack/279. Perfect Squares.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int num = 1; num <= n; num++) {
+
+            for (int sq = 1; sq * sq <= num; sq++) {                             <-- perfect squares ≤ num: 1², 2², 3²
+                if (num - sq * sq < 0 || dp[num - sq*sq] == INT_MAX) continue;
+                dp[num] = min(dp[num], dp[num - sq*sq] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+};
